d818a2d0c03aee86612b10819b94708b729d78bd
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-06-06 19:23:51 +0530

    add sync all tokens

--- src/server-extension/resolvers/sync_collections.ts
+++ src/server-extension/resolvers/sync_collections.ts
@@ -1,15 +1,21 @@
 /* eslint-disable max-classes-per-file */
 import { Query, Resolver } from 'type-graphql'
 import 'reflect-metadata'
+import { EntityManager, IsNull } from 'typeorm'
 import { syncAllCollections } from '../../jobs/collection-stats'
+import { Token } from '../../model'
+import { processMetadata } from 'src/jobs/process-metadata'
 
-const mins10 = 1000 * 60 * 30
+const mins10 = 1000 * 60 * 10
 let rateLimit: number | null = null
 
 @Resolver()
 export class SyncCollectionsResolver {
+    constructor(private tx: () => Promise<EntityManager>) {}
+
     @Query(() => Boolean)
     async syncCollections(): Promise<boolean> {
+        const manager = await this.tx()
         if (rateLimit) {
             const timeLeft = Math.ceil((rateLimit + mins10 - Date.now()) / 1000)
 
@@ -19,6 +25,16 @@ export class SyncCollectionsResolver {
         }
 
         rateLimit = Date.now()
+
+        const tokens = await manager.find(Token, {
+            where: { metadata: IsNull() },
+            select: ['id'],
+            order: { id: 'ASC' },
+        })
+
+        tokens.forEach((token) => {
+            processMetadata(token.id, 'token', true)
+        })
 
         await syncAllCollections()
 
